ee7e45e42294af6456ee027d0430db1c8c9059d1
sg5717 <sg5717@stern.nyu.edu>
2019-05-11 21:05:48 -0400

    Update run.py
    Added parenthesis in print command in line 19 and 20

--- run.py
+++ run.py
@@ -16,8 +16,8 @@ PORT = os.getenv('PORT', '5000')
 ######################################################################
 if __name__ == "__main__":
     print ("****************************************")
-    print " P R O D U C T   S E R V I C E   R U N N I N G"
-    print "****************************************"
+    print (" P R O D U C T   S E R V I C E   R U N N I N G")
+    print ("****************************************")
     service.initialize_logging()
     service.init_db()  # make our sqlalchemy tables
     app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
