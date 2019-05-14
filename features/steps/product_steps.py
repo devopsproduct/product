9b3732dc198f3a73302150939decc5f1f0773dff
MP <themp731@gmail.com>
2019-05-14 00:28:15 -0400

    Adding Wait for webdriver
    I think the requests are happening too fast for sellenium to work

--- features/steps/product_steps.py
+++ features/steps/product_steps.py
@@ -92,15 +92,15 @@ def step_impl(context, button):
 
 @then('I should see "{name}" in the results')
 def step_impl(context, name):
+    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
+    expect(found).to_be(True)
     element = context.driver.find_element_by_id('search_results')
     expect(element.text).to_contain(name)
-    # found = WebDriverWait(context.driver, WAIT_SECONDS).until(
-    #     expected_conditions.text_to_be_present_in_element(
-    #         (By.ID, 'search_results'),
-    #         name
-    #     )
-    # )
-    # expect(found).to_be(True)
 
 @then('I should not see "{name}" in the results')
 def step_impl(context, name):
